10489ed3f3189180f00d46b5c485c96599716a6d
Alessandro-Fiora <fiora.ale97@gmail.com>
2024-10-24 17:31:00 +0200

    pulizia e commenti js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,13 @@
-// Descrizione:
-// Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
-// Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
-// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-// Potete usare liste, input e bottoni non stilizzati, mettendo stampe di debug in console e risultato finale in un alert.
-// Solo una volta finito, a piacere e facoltativamente, potete aggiungete lo stile.
-// NOTA:  non è importante l'ordine con cui l'utente inserisce i numeri, basta che ne indovini il più possibile.
-// BONUS
-// Inseriamo la validazione: se l'utente mette due numeri uguali o inserisce cose diverse da numeri lo blocchiamo in qualche modo.
-// Se l’utente ha inserito qualcosa di non valido, segnaliamolo visivamente nel form.
-// Consigli del giorno
-// Pensate prima in italiano.
-// Dividete in piccoli problemi la consegna.
-// Individuate gli elementi di cui avete bisogno per realizzare il programma.
-// Immaginate la logica come fosse uno snack: "Dati 2 array di numeri, indica quali e quanti numeri ci sono in comune tra i due array"
-
-// Al caricamento della pagina genero 5 numeri casuali (da 1 a 99), li salvo in un array e li stampo in pagina
+// * Alert con le istruzioni
 alert(
   'Simon Says!\nHai 30 secondi per memorizzare 5 numeri\nInseriscili nei campi e schiaccia "Conferma" per vedere quanti ne hai indovinati.\nSe non sei soddisfatto del risultato puoi sempre riprovare!'
 );
 
+// ! ------------------------------------   TUTTE LE FUNZIONI
+/**
+ * Generates an array of 5 random numbers between 1 and 99
+ * @returns {Array} the array of random numbers
+ */
 const randomNumberArrayGenerator = () => {
   const randomArray = [];
   for (let i = 0; i < 5; i++) {
@@ -27,14 +16,24 @@ const randomNumberArrayGenerator = () => {
   }
   return randomArray;
 };
-const printArray = () => {
+/**
+ * Prints the elements of the given array into the indicated positions
+ * @param {Array} arrayToPrint Array of values to print
+ * @param {Array} arrayOfNodesToPrintInto Array of objects in which values to print into
+ */
+const printArray = (arrayToPrint, arrayOfNodesToPrintInto) => {
   // per ogni slot, inserisco nel testo html il numero casuale corrispondente
-  slotArray.forEach((currentSlot, index) => {
-    currentSlot.innerHTML = `<span>${SimonNumbers[index]}</span>`;
+  arrayOfNodesToPrintInto.forEach((currentSlot, index) => {
+    currentSlot.innerHTML = `<span>${arrayToPrint[index]}</span>`;
   });
 };
-const printInputs = () => {
-  slotArray.forEach((currentSlot, index) => {
+/**
+ * Generates input tags and for each of the elements of the given array it replaces it with an input
+ * @param {Array} whereToPrint Array of nodes where to put input tags
+ */
+const printInputs = (whereToPrint) => {
+  // Per ognuno degli slot sostituisco i numeri con degli input
+  whereToPrint.forEach((currentSlot, index) => {
     currentSlot.innerHTML = `<input
         type="number"
         class="form-control"
@@ -43,40 +42,49 @@ const printInputs = () => {
         max="99"
         required
         />`;
+    // Recupero il nodo dei bottoni
+    const buttonsEl = document.getElementById("buttons");
+    // Tolgo la classe display none per mostrarli in pagina
     buttonsEl.classList.remove("d-none");
   });
 };
 const countdownHandler = () => {
   // Recupero nodo dove piazzare il countdown
   const countdownEl = document.getElementById("remaining-time");
+  // Se il countdown è maggiore di 0
   if (countdown > 0) {
+    // Stampo in pagina i secondi rimasti
     countdownEl.innerText = countdown;
+    // Decremento i secondi
     countdown--;
   } else {
+    // Se il countdown è 0 allora lo lascio settato a 0
     countdownEl.innerText = 0;
-    printInputs();
+    // Sostituisco i numeri con degli input
+    printInputs(slotArray);
+    // Fermo il conto alla rovescia
     clearInterval(countdownInterval);
   }
 };
-// creo array di numeri casuali
-const SimonNumbers = randomNumberArrayGenerator();
-// Recupero i nodi che mi servono
-let slot1 = document.getElementById("slot-1");
-let slot2 = document.getElementById("slot-2");
-let slot3 = document.getElementById("slot-3");
-let slot4 = document.getElementById("slot-4");
-let slot5 = document.getElementById("slot-5");
-const buttonsEl = document.getElementById("buttons");
-const inputForm = document.getElementById("input-form");
-const retryButton = document.getElementById("retry-button");
+/**
+ * Gets nodes where to print numbers or inputs in and returns an array of these elements
+ * @returns {Array} array of nodes where to print numbers or inputs
+ */
+const getSlotElements = () => {
+  // Recupero i nodi che mi servono
+  const slot1 = document.getElementById("slot-1");
+  const slot2 = document.getElementById("slot-2");
+  const slot3 = document.getElementById("slot-3");
+  const slot4 = document.getElementById("slot-4");
+  const slot5 = document.getElementById("slot-5");
 
-// creo array di nodi
-let slotArray = [slot1, slot2, slot3, slot4, slot5];
-
-printArray();
-// Inizializzo countdown
-let countdown = 2;
-
+  // li inserisco in un array di nodi
+  return [slot1, slot2, slot3, slot4, slot5];
+};
+/**
+ * Gets input nodes and returns an array of these elements
+ * @returns {Array} array of nodes of the inputs in html
+ */
 const getInputValues = () => {
   input1 = document.getElementById("number-input0");
   input2 = document.getElementById("number-input1");
@@ -87,6 +95,12 @@ const getInputValues = () => {
   inputArray = [input1, input2, input3, input4, input5];
   return inputArray;
 };
+/**
+ * Checks if each element of an array is included in another array, it returns an array of the elements that are included
+ * @param {Array} arrayToCheck Array of elements to check
+ * @param {Array} whitelist Witelist array to check into
+ * @returns {Array} Array of elements in the array to check that are included in whitelist
+ */
 const checkGuesses = (arrayToCheck, whitelist) => {
   //   let counter = 0;
   const guessedNumbers = [];
@@ -101,24 +115,52 @@ const checkGuesses = (arrayToCheck, whitelist) => {
   });
   return guessedNumbers;
 };
-// Parte un timer di 30 secondi
-// Allo scadere del timer sostituisco i numeri con degli input numerici
-const countdownInterval = setInterval(countdownHandler, 1000);
-// All'invio del form controllo ad uno ad uno se i numeri inseriti sono contenuti nell'arrray dei numeri casuali
-inputForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const checkGuessHandler = () => {
-    const inputArray = getInputValues();
-    const guessed = checkGuesses(inputArray, SimonNumbers);
+const checkGuessHandler = () => {
+  // Recupero gli input e li metto in un array
+  const inputArray = getInputValues();
 
-    outputTextEl = document.getElementById("output-text");
-    outputTextEl.innerText = `Ne hai indovinati ${guessed.length} : ${guessed}
-    Riprova!`;
-  };
+  // Controllo le corrispondenze
+  const guessed = checkGuesses(inputArray, SimonNumbers);
+
+  // Recupero il nodo dove stampare il testo di output
+  outputTextEl = document.getElementById("output-text");
+
+  // Ci inserisco il testo di output
+  outputTextEl.innerText = `Ne hai indovinati ${guessed.length} : ${guessed}
+  Riprova!`;
+};
+// ! ------------------------------------   -----------------
+
+// * Al caricamento della pagina genero 5 numeri casuali (da 1 a 99) e li salvo in un array
+const SimonNumbers = randomNumberArrayGenerator();
+
+// * Creo array di nodi dove stampare i numeri dell'array e in seguito gli inputs
+const slotArray = getSlotElements();
+
+// * Stampo i numeri generati
+printArray(SimonNumbers, slotArray);
+
+// * Inizializzo un timer di 30 secondi (1 secondo prima che venga eseguito l'intervallo e 29 intervalli)
+let countdown = 1;
+
+// * Stampo il countdown in pagina e allo scadere del timer sostituisco i numeri con degli input
+const countdownInterval = setInterval(countdownHandler, 1000);
+
+// * Recupero il nodo del form
+const inputForm = document.getElementById("input-form");
+
+// * All'invio del form (pressione sul bottone conferma)
+inputForm.addEventListener("submit", (e) => {
+  // Prevengo l'invio dei dati al back-end e il refresh della pagina
+  e.preventDefault();
+  // * Controllo ad uno ad uno se i numeri inseriti sono contenuti nell'arrray dei numeri casuali e stampo l'output in pagina
   checkGuessHandler();
 });
+
+// * Recupero il nodo del bottone 'Riprova'
+const retryButton = document.getElementById("retry-button");
+
+// * Alla pressione del bottone 'Riprova' ricarico la pagina
 retryButton.addEventListener("click", () => {
   location.reload();
 });
-
-// Stampo il risultato in pagina
